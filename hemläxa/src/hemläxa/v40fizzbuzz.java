3175ecf4daf60aabd9fdc641323714cc764d2435
Mattias-M <mattias.martensson@elev.ga.ntig.se>
2019-10-07 09:54:29 +0200

    FizzBuzz

---
+++ hemläxa/src/hemläxa/v40fizzbuzz.java
@@ -0,0 +1,28 @@
+package hemläxa;
+
+import java.util.Scanner;
+
+public class v40fizzbuzz {
+
+	public static void main(String[] args) {
+		
+Scanner scan = new Scanner(System.in);
+		
+		System.out.println("skriv 3 tal");
+		int X = scan.nextInt();
+		int Y = scan.nextInt();
+		int N = scan.nextInt();
+		
+		for (int i=1; i<=N; i++ ) {
+			if (i % X == 0 && i % Y == 0)
+				System.out.println("FizzBuzz");
+			else if( X % i ==0 ) System.out.println("Fizz");
+			else if( Y % i ==0 ) System.out.println("Buzz");
+			else System.out.println(i);
+		
+		
+
+	}
+
+	}
+}
