4b8b9bc373139336194a0012ae76b167b0295109
Mattias-M <mattias.martensson@elev.ga.ntig.se>
2019-10-24 10:31:52 +0200

    hemläxa

---
+++ hemläxa/src/hemläxa/metod2.java
@@ -0,0 +1,15 @@
+package hemläxa;
+
+import java.util.Scanner;
+
+public class metod2 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		int x 
+
+	}
+
+	public static void min(int x, int y)
+}
---
+++ hemläxa/src/hemläxa/v43.java
@@ -0,0 +1,22 @@
+package hemläxa;
+
+import java.util.Scanner;
+
+public class v43 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		double radius = scan.nextInt();
+		
+		volume(double radius);
+		
+	}
+	
+	public static void volume(double radius) {
+		
+		(4*Math.PI*radius Math.pow(radius, 3)/3)
+		
+	}
+}
