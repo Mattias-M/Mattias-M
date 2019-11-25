66d219144f28c806bdc30f6f590b211a28dfea6d
Mattias-M <mattias.martensson@elev.ga.ntig.se>
2019-11-25 09:51:51 +0100

    har gjort en class för frågorna och gjorde den fösrta frågan;

---
+++ hemläxa/src/hemläxa/frågor.java
@@ -0,0 +1,13 @@
+package hemläxa;
+
+
+
+public class frågor {
+
+	public static void main(String[] args) {
+	
+		System.out.println("60 dm^3 järn väger " + uppgift.volumeToMassSolid(SolidTable.IRON, 30));
+
+	}
+
+}
