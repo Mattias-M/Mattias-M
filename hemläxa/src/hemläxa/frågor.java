cfbe5c3944db8b0daaf20b295ca1db36fa3d7a06
Mattias-M <mattias.martensson@elev.ga.ntig.se>
2019-11-25 10:23:07 +0100

    gjorde frågorna 1, 2 och 3

--- hemläxa/src/hemläxa/frågor.java
+++ hemläxa/src/hemläxa/frågor.java
@@ -7,6 +7,10 @@ public class frågor {
 	public static void main(String[] args) {
 	
 		System.out.println("60 dm^3 järn väger " + uppgift.volumeToMassSolid(SolidTable.IRON, 30));
+		
+		System.out.println("Tomas löper " + uppgift.svtDistance(2.7, 3000)+"m");
+		
+		System.out.println("det tar " + uppgift.heatFluid(FluidTable.WATER, 4, FluidTable.WATER.boilPoint ));
 
 	}
 
