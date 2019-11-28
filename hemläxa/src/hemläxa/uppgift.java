a0befd67d587fc48d9e96259e87e95cf1d8f9770
Mattias-M <mattias.martensson@elev.ga.ntig.se>
2019-11-28 11:50:52 +0100

    fråga 6 och lite java doc

--- hemläxa/src/hemläxa/uppgift.java
+++ hemläxa/src/hemläxa/uppgift.java
@@ -20,21 +20,38 @@ public class uppgift {
 		System.out.println(pressureUnderWater(75));
 		
 		System.out.println(velocityToHeight(16.667));
+		
+		System.out.println(power(work(force(735),(100/3.6 * 4.8)),4.8));
+		
+		
 	}
-
+	/**
+	 * gör fahrenheit till Celsius
+	 * @param fahrenheit värdet för tempraturen i fahrenheit
+	 * @return sickar tillbaka tempraturen i celsius
+	 */
 	public static double fahrenheitToCelsius(double fahrenheit) {
 
 		double celsius = ((fahrenheit - 32)*5)/9;
 		
 		return celsius;
 	}
-	
+	/**
+	 * ändrar tempratur från kelvin till celsius
+	 * @param kelvin värdet i tempraturen i kelvin.
+	 * @return
+	 */
 	public static double kelvinToCelsius(double kelvin) {
 		double celsius = (kelvin - 273.15);
 		
 		return celsius;
 	}
-	
+	/**
+	 * räknar ut trycket under flytande ämne.
+	 * @param fluid vilken vätska
+	 * @param deep hur långt ner i vätska.
+	 * @return trycket
+	 */
 	public static double fluidPressure(FluidTable fluid, double deep) {
 
 		double thePressure = 0;
@@ -43,26 +60,44 @@ public class uppgift {
 		return thePressure;
 		
 	}
-	
+	/**
+	 * räknar ut trycket under vatten vid en viss djup.
+	 * @param deep djupet under vattnet
+	 * @return trycket
+	 */
 	public static double pressureUnderWater(double deep) {
 		double thePressure1 = FluidTable.WATER.density * g_swe * deep;
 		
 		return thePressure1;
 	}
-	
+	/**
+	 *räknar ut kinetiskenergi  
+	 * @param mass massan på obejektet
+	 * @param velocity hasstigeheten
+	 * @return kinetiscenegin.
+	 */
 	public static double kineticEnergy(double mass, double velocity) {
-		Math math;
 		double KE = 0.5 * mass * Math.pow(velocity, 2);
 		return KE;
 		
 	}
+	/**
+	 * räknar ut potensielenergi
+	 * @param mass massa på obejektet
+	 * @param height höjden 
+	 * @return potensielenegi
+	 */
 	public static double potentialEnergy(double mass, double height) {
 		
 		double ep = g_swe * mass * height;
 		return ep;
 		
 	}
-	
+	/**
+	 * räknar ut hastigheten på obejekt när man släppps i fritt fall
+	 * @param height höjden som den släpps från.
+	 * @return
+	 */
 	public static double fallSpeed(double height) {
 		
 		double hastighet = Math.sqrt(2*g_swe*height);
@@ -124,4 +159,18 @@ public class uppgift {
 	public static double velocityToHeight(double velocity) {
 		return Math.pow(velocity, 2)/ (2*g_swe);
 	}
+	
+	public static double HookesLag (double k, double deltaL) {
+		return k*deltaL;
+	}
+	
+	public static double ljudintensitenten (double effekt, double area) {
+		return effekt/area;
+	}
+	
+	public static double force (double mass) {
+		return mass*g_swe;
+	}
+
+	
 }
