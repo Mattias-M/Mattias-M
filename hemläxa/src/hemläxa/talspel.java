fdcd85830b5677d1cd956f37b8aa4f7217e0423c
Mattias-M <mattias.martensson@elev.ga.ntig.se>
2020-03-23 18:25:53 +0100

    halvägs med javadoc

--- hemläxa/src/hemläxa/talspel.java
+++ hemläxa/src/hemläxa/talspel.java
@@ -3,7 +3,9 @@ package hemläxa;
 import java.util.Random;
 import java.util.Scanner;
 
+
 public class talspel {
+	// detta är alla variablar jag anväder mig av som ska vara i flrér eän en metod.
 	static Random rand = new Random();
 	static Scanner scan = new Scanner(System.in);
 	static String Nickname;
@@ -15,21 +17,37 @@ public class talspel {
 		
 		ChoseName();
 	}
-	
+	/**
+	 * Denna metoden gjör så man kan välja namn och startar intro
+	 */
 	public static void ChoseName() {
 		System.out.println("Hej och välkomen till Talspelet! skriv in ditt namn.");
 		Nickname = scan.nextLine();
 		Intro();
-		
+	/**
+	 * Metoden berättar hur spelet funkar med en sysout. Den startar näsra metod som är ChoseGuesses 
+	 */
 	}
 	public static void Intro() {
 		System.out.println("Hej " + Nickname + " välkomen till Talspelaet. I det spelet så kommer du försöka gissa på ett tal som du inte vet vad det är. När du har gissat så kommer då få ledtrådar om du gissa för högt eller för lågt. Lycka till! ");
 		ChoseGuesses();
+	/**
+	 * MEtoden gör så man får bestämma hur många gissningar man har på sig.
+	 * guesses är antal gissnigar man har.
+	 * Sen startar den nästa metod som är ChoseNumber.
+	 */
 	}
 	public static void ChoseGuesses() {
-		System.out.println("Nu ska du välja hur mycket gissnigar du har på dig att gissa talet. SKriv hur många gissnigar du vill ha.");
+		System.out.println("Nu ska du välja hur mycket gissnigar du har på dig att gissa talet. Skriv hur många gissnigar du vill ha.");
 		guesses = IntChecker();
 		ChoseNumber();
+	/**
+	 *
+	 * Denna metoden gör så man kan bestämma hur hög det högsta tillet kan bli.
+	 * anväder mig av packaget Random för att slumpmässigt ta ett tal. Så det packaget har jag inportart.
+	 * rand.nextInt(maxNumber) välger slupmsägt tal mellan 0-maxNumber
+	 * till slut så startar den nästa metod Guess
+	 */
 	}
 	public static void ChoseNumber() {
 		System.out.println("Nu ska du välja hur stort talet som du ska gissa på ska vara. Det minsta kommer alltid vara 0. Så om du skriver 100 så kommer talet vara någonstans emellan 0 til 99");
@@ -37,7 +55,10 @@ public class talspel {
 		int maxNumber = IntChecker();
 		secretnumber = rand.nextInt(maxNumber);
 		Guess();
-		
+	/**
+	 * Metoden gör så att spelaren kan gissa på talet.
+	 * Om spelaren gissar för högt så 
+	 */
 	}
 	public static void Guess() {
 		System.out.println("nu är det dax att giss!");
