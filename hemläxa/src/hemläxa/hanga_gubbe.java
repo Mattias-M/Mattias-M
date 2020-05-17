a737da04d2f3aaf0f8cdb6d91da1c6dd638670b1
Mattias-M <mattias.martensson@elev.ga.ntig.se>
2020-05-17 14:17:32 +0200

    skrivit komentarer

--- hemläxa/src/hemläxa/hanga_gubbe.java
+++ hemläxa/src/hemläxa/hanga_gubbe.java
@@ -14,15 +14,19 @@ public class hanga_gubbe {
 	static char charInWord;
 	static String guess;
 	static String theSecretWord;
-	public static String[] easyWords = {"JAVA", "HEJ", "ARE", "KATT", "MAMMA" };
+	public static String[] easyWords = { "JAVA", "HEJ", "ARE", "KATT", "MAMMA" };
 	public static String[] hardWords = { "HEJSAN", "CITERA", "MYNDIG", "DIAFRAGMA", "REGRESS" };
 	public static char[] charArray = {};
 	public static ArrayList<Character> theSecretWordBut_ = new ArrayList<Character>();
 
+// metoden startar spelet.
 	public static void main(String[] args) {
 		welcome();
 	}
 
+	/**
+	 * välkomnar spelaren och förklarar reglerna, sedan startar nästa metod
+	 */
 	static void welcome() {
 		System.out.println("Hej, och välkomen till hänga gubbespelet.");
 		System.out.println("I Detta spelet ska du gissa vilka bokstäver ditt finn i ett ord.");
@@ -30,6 +34,11 @@ public class hanga_gubbe {
 		choseDeifficulty();
 	}
 
+	/**
+	 * här så väljer spelaren vilken svårighetsgrad. om spelaren ska skriv 1 eller 2
+	 * beroend vilken svårighetsgran spelaren vill ha. finn också en try-chatch om
+	 * spelaren inte skriver sifror.
+	 */
 	static void choseDeifficulty() {
 		System.out.println("Nu ska du välja svårighetsgrad, skriv 1 om du vill ha lätta ord, eller 2 för svåra ord.");
 		int hardOrEasy = IntChecker();
@@ -43,16 +52,13 @@ public class hanga_gubbe {
 			System.out.println("Du måste skriva 1 eller 2 inget annat.");
 			choseDeifficulty();
 		}
-		scannerListToArrayList();
-	}
-
-	static void scannerListToArrayList() {
-
 		randomizer();
-
 	}
 
-
+	/**
+	 * denna metoden tar slumpmässigt ord från arryen easywords om spelaren vade
+	 * difficulty easy. Sedan startar den metoden maketheSecretWordBut_
+	 */
 	static void randomizer() {
 		Random random = new Random();
 
@@ -62,36 +68,53 @@ public class hanga_gubbe {
 			theSecretWord = hardWords[random.nextInt(hardWords.length)];
 		}
 
-		maketheSecretWordBut_();
+		makeTheSecretWordBut_();
 
 	}
-	
-	static void maketheSecretWordBut_() {
-		for (int i= 0; i < theSecretWord.length(); i++) {
+
+	/**
+	 * metoden lägger till så många undetsträck i arrryListan theSecretWordBut_ som
+	 * theSecretWord är långt.
+	 */
+	static void makeTheSecretWordBut_() {
+		for (int i = 0; i < theSecretWord.length(); i++) {
 			theSecretWordBut_.add('_');
 		}
 		theGame();
 	}
 
+	/*
+	 * dena metoden sätter spelarens liv till 9
+	 */
 	static void theGame() {
 		playerLife = 9;
-		
+
 		graphic();
-		
+
 	}
 
+	/**
+	 * här så visas theSecretWordBut_ och hur många liv spelaren har
+	 */
 	static void graphic() {
-	
-		
-		
+
 		System.out.println(theSecretWordBut_);
-		
+
 		System.out.println("du har " + playerLife + " liv kvar.");
-		
+
 		guess();
 
 	}
 
+	/**
+	 * först så berättar spelet att spelaren ska gissa. När spelaren gissar så är
+	 * det en string, gör denna string till storabokstöver med tuUpperCase. sedan så
+	 * delar metoden upp gissnigen till chars i en array. sedan kollar den upp
+	 * bokstav för bokstav om bokstaven är i ordet. Om bokstaven är i ordet så
+	 * startaren den metoden checkWhereCharIsInString om bokstaven inte är i ordet
+	 * så tar den bort ett liv från spelaren och statar metoden checkIfPlayerDead()
+	 * sedan när allt är klart så startr metode graphic.
+	 */
 	static void guess() {
 		System.out.println("Nu är det dax att gissa på bokstäver. Du kan gissa på hur många bokstäver du vill.");
 		String guess = scan.nextLine();
@@ -99,12 +122,11 @@ public class hanga_gubbe {
 
 		char[] charArray = guess.toCharArray();
 
-
 		for (char i : charArray) {
 
 			if (theSecretWord.indexOf(i) != -1) {
 				charInWord = i;
-				System.out.println(charInWord +" är i ordet");
+				System.out.println(charInWord + " är i ordet");
 				checkWhereCharIsInString();
 
 			} else {
@@ -117,43 +139,42 @@ public class hanga_gubbe {
 		graphic();
 	}
 
+	/**
+	 * här så kollar om spelaren är död, om den är det så startar metoden end annar
+	 * så händer inget.
+	 */
 	static void checkIfPlayerDead() {
 		if (playerLife == 0) {
 			end();
 		} else {
-			
+
 		}
 	}
 
+	/**
+	 * denna metoden kollar vart bokstaven som spelaren gissa på är i ordet. Sedan så lägges bokstaven i theSecretWordBut_ där den ska vara.
+	 */
 	static void checkWhereCharIsInString() {
 		for (int i = 0; i < theSecretWord.length(); i++) {
-			
-			if(charInWord == theSecretWord.charAt(i)) {
-				
+
+			if (charInWord == theSecretWord.charAt(i)) {
+
 				theSecretWordBut_.set(i, charInWord);
-								
+
 				System.out.println(theSecretWordBut_);
-				
+
 				checkIfWordIsDon();
 			}
-			
-			
-			
-			
-			//charInWord = theSecretWord.charAt(i);
-			// se om charInWord = först char i ordet theSecretWord
-			// om det är så byt ut första _ i variablen the_word
-			// om inte så se om charInWord = andra char i ordet theSecretWord
-			// om det är så byt ut andra _ i variablen the_word.
-			// håller på så tills man har kolla igenom alla char i theSecretWord
 
 		}
 	}
-
+/**
+ * 
+ */
 	static void checkIfWordIsDon() {
 
 		if (theSecretWordBut_.indexOf('_') != -1) {
-			
+
 		} else {
 			System.out.println("Grattis, du klarade det!!");
 			end();
@@ -164,10 +185,9 @@ public class hanga_gubbe {
 	static void end() {
 		System.out.println("vill du spela ingen? skriv 1 annars skriv 2");
 		int YesOrNo = IntChecker();
-		if(YesOrNo == 1) {
+		if (YesOrNo == 1) {
 			choseDeifficulty();
-		}
-		else {
+		} else {
 			System.exit(0);
 		}
 
